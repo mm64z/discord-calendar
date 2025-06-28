341d3f6d191016a730fac13b9bd56e409af57006
Michael Matthews <michael.64.matthews@gmail.com>
2025-06-28 00:24:16 -0400

    Updates

---
+++ assets/index-yKt9sliS.js
@@ -0,0 +1,6 @@
+(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function s(e){if(e.ep)return;e.ep=!0;const r=i(e);fetch(e.href,r)}})();const c="/discord-calendar/assets/rocket-BNemYsaC.png";document.querySelector("#app").innerHTML=`
+  <div>
+    <img src="${c}" class="logo" alt="Discord" />
+    <h1>Hello, World!</h1>
+  </div>
+`;
